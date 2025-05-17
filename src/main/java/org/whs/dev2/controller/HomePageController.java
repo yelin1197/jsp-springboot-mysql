fb5f17819e4458eb10fd65b7641345e4d91db9d5
yelin1197 <yelin1197@ewhain.net>
2025-05-18 01:23:26 +0900

    fix: db 설정 변경

---
+++ src/main/java/org/whs/dev2/controller/HomePageController.java
@@ -0,0 +1,12 @@
+package org.whs.dev2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index"; // → /WEB-INF/views/index.jsp 로 매핑됨
+    }
+}
